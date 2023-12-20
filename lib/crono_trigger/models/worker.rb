4221e5c1e532b555e9db6a50dd5c2c3f78dde748
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2023-12-20 19:17:07 +0900

    Pass coder class via keyword_args for Rails 7.1 and later
    https://github.com/rails/rails/blob/v7.1.0/activerecord/CHANGELOG.md#:~:text=Jean%20Boussier-,ActiveRecord%3A%3ABase.serialize%20signature,two%20distinct%20keyword%20arguments

--- lib/crono_trigger/models/worker.rb
+++ lib/crono_trigger/models/worker.rb
@@ -8,7 +8,12 @@ module CronoTrigger
       ALIVE_THRESHOLD = CronoTrigger::Worker::HEARTBEAT_INTERVAL * 5
 
       enum executor_status: {running: "running", quiet: "quiet", shuttingdown: "shuttingdown", shutdown: "shutdown"}
-      serialize :polling_model_names, JSON
+
+      if ActiveRecord.version >= Gem::Version.new("7.1.0")
+        serialize :polling_model_names, coder: JSON
+      else
+        serialize :polling_model_names, JSON
+      end
 
       scope :alive_workers, proc { where(arel_table[:last_heartbeated_at].gteq(Time.current - ALIVE_THRESHOLD)) }
     end
